0f9497e46c03eee544cdebde17628109cf037ece
jamesn <afeIfz.drtgcE@what.net>
2017-10-04 12:13:01 -0500

    update pathing.

--- lib/splunk_client/splunk_job.rb
+++ lib/splunk_client/splunk_job.rb
@@ -2,7 +2,7 @@
 # Original Repo:: https://github.com/cbrito/splunk-client
 
 require File.expand_path File.join(File.dirname(__FILE__), 'splunk_results')
-require 'splunk_exceptions'
+require File.expand_path File.join(File.dirname(__FILE__), 'splunk_exceptions')
 
 
 class SplunkJob
